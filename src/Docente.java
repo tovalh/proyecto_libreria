42d753e8a06e02301521e8d3d67eeb643babe1da
MacpByte <m.cortspiones@uandresbello.edu>
2025-11-10 17:49:21 -0300

    Docente.java

--- src/Docente.java
+++ src/Docente.java
@@ -1,26 +1,16 @@
-public class Docente extends Usuario{
+public class Docente extends Usuario {
     private String profesion;
     private String grado;
 
-    public Docente(String nombre_completo, String rut, char genero, String carrera, String profesion, String grado) {
-        super(nombre_completo, rut, genero, carrera);
+    public Docente(String nombre_completo, String rut, char genero,
+                   String profesion, String grado) {
+        super(nombre_completo, rut, genero);
         this.profesion = profesion;
         this.grado = grado;
     }
 
-    public String getProfesion() {
-        return profesion;
-    }
-
-    public void setProfesion(String profesion) {
-        this.profesion = profesion;
-    }
-
-    public String getGrado() {
-        return grado;
-    }
-
-    public void setGrado(String grado) {
-        this.grado = grado;
-    }
+    public String getProfesion() { return profesion; }
+    public void setProfesion(String profesion) { this.profesion = profesion; }
+    public String getGrado() { return grado; }
+    public void setGrado(String grado) { this.grado = grado; }
 }
