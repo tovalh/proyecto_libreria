621d89bdd296c579c3c175109e489cb541eb8af9
MacpByte <m.cortspiones@uandresbello.edu>
2025-11-10 17:48:59 -0300

    Estudiante.java

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,7 +1,12 @@
-public class Estudiante extends Usuario{
+public class Estudiante extends Usuario {
+    private String carrera;
 
-    public Estudiante(String nombre, String run, char genero, String carrera) {
-        super(nombre, run, genero, carrera);
+    // usa "rut" para ser consistente
+    public Estudiante(String nombre, String rut, char genero, String carrera) {
+        super(nombre, rut, genero);
+        this.carrera = carrera;
     }
-}
 
+    public String getCarrera() { return carrera; }
+    public void setCarrera(String carrera) { this.carrera = carrera; }
+}
