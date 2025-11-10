1942c26f522ee0e38a519fdd13371fef6aa28f04
MacpByte <m.cortspiones@uandresbello.edu>
2025-11-10 17:46:39 -0300

    Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -2,14 +2,12 @@ public class Usuario {
     private String nombre_completo;
     private String rut;
     private char genero;
-    private String carrera;
     private String prestamo;
 
-    public Usuario(String nombre_completo, String rut, char genero, String carrera) {
+    public Usuario(String nombre_completo, String rut, char genero) {
         this.nombre_completo = nombre_completo;
         this.rut = rut;
         this.genero = genero;
-        this.carrera = carrera;
         this.prestamo = "0";
     }
 
@@ -17,54 +15,27 @@ public class Usuario {
         this.prestamo = "0";
     }
 
-    public String getNombre_completo() {
-        return nombre_completo;
+    public String getNombre_completo() { return nombre_completo; }
+    public void setNombre_completo(String nombre_completo) { this.nombre_completo = nombre_completo; }
+
+    public String getRut() { return rut; }
+    public void setRut(String rut) { this.rut = rut; }
+
+    public char getGenero() { return genero; }
+    public void setGenero(char genero) { this.genero = genero; }
+
+    public String getPrestamo() { return prestamo; }
+    public void setPrestamo(String prestamo) { this.prestamo = prestamo; }
+
+    public boolean validar_genero(char genero) {
+        return genero == 'M' || genero == 'F';
     }
 
-    public void setNombre_completo(String nombre_completo) {
-        this.nombre_completo = nombre_completo;
-    }
+    // Validar RUT
+    public static boolean validarRut(String rut) {
+        if (rut == null || rut.isEmpty()) return false;
 
-    public String getRut() {
-        return rut;
-    }
-
-    public void setRut(String rut) {
-        this.rut = rut;
-    }
-
-    public char getGenero() {
-        return genero;
-    }
-
-    public void setGenero(char genero) {
-        this.genero = genero;
-    }
-
-    public String getCarrera() {
-        return carrera;
-    }
-
-    public void setCarrera(String carrera) {
-        this.carrera = carrera;
-    }
-
-    public String getPrestamo() {
-        return prestamo;
-    }
-
-    public void setPrestamo(String prestamo) {
-        this.prestamo = prestamo;
-    }
-
-    public boolean validar_genero(char genero){
-        if(genero=='M'){
-            return true;
-        } else if(genero=='F'){
-            return true;
-        } else {
-            return false;
-        }
+        String clean = rut.replace(".", "").trim().toUpperCase();
+        return clean.matches("\\d{7,8}-[0-9K]");
     }
 }
-
